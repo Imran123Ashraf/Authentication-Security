ca61785dc92b72be715a822d6d7bf5eac1da4438
Imran Ashraf <imranashraf23081999@gmail.com>
2022-09-17 03:34:45 +0530

    Add Environment Vars

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express= require("express");
 const bodyParser= require("body-parser");
 const ejs= require("ejs");
@@ -17,8 +18,8 @@ const userSchema= new mongoose.Schema({
     password: String
 });
 
-const secret= "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
